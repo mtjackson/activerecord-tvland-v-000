d0c59b81cc163f3e528a231852711237c54eaf06
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-04 21:49:50 +0000

    automatically backed up by learn

--- db/migrate/008_add_data_to_show.rb
+++ db/migrate/008_add_data_to_show.rb
@@ -3,5 +3,6 @@ class AddDataToShow < ActiveRecord::Migration[4.2]
     add_column :shows, :day, :string
     add_column :shows, :season, :string
     add_column :shows, :genre, :string
+    add_column :shows, :network_id, :integer
   end
 end

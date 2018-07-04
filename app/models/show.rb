6f41e2aa05572c390c451da4c508c1be79423daf
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-04 21:50:57 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,6 +2,7 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
 
+  def network
   def build_network(call_letters)
     self.network = Network.find_by(call_letters)
   end
